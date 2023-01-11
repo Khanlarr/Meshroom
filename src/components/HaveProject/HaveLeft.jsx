0e3d9d5aa5f4e5ff7fe4f7b0cf17fa780d973a9a
Khanlarr <xanlar.beybalayev@gmail.com>
2023-01-11 17:04:29 +0400

    border-bottom change

--- src/components/HaveProject/HaveLeft.jsx
+++ src/components/HaveProject/HaveLeft.jsx
@@ -1,67 +1,107 @@
-import React,{useState} from 'react'
-import inputbg from '../../assets/img/Blackborderinput.svg'
+import React, { useState } from "react";
+import inputbg from "../../assets/img/Blackborderinput.svg";
 
 function HaveLeft() {
-    const [width,setWidth]=useState(true)
-    const changeWidth=()=>{
-        if(window.screen.width>=1024){
-          setWidth(false)
-        }
-        else{
-            setWidth(true)
-        }
+  const [width, setWidth] = useState(true);
+  const [span1, setSpan1] = useState(false);
+  const [span2, setSpan2] = useState(false);
+  const [span3, setSpan3] = useState(false);
+  const changeWidth = () => {
+    if (window.screen.width >= 1024) {
+      setWidth(false);
+    } else {
+      setWidth(true);
     }
-    window.addEventListener('scroll',changeWidth)
-    return (
-        <div data-aos={width?"zoom-in":"fade-right"} className='haveLeft'>
-            <div  className="haveLeftHeading">
-                <div>
-                    <h2>have a project
-                        in mind?</h2>
-                </div>
-                <div>
-                    <p>Talk a little bit about your project and we will contact you</p>
-                </div>
-            </div>
+  };
+  window.addEventListener("scroll", changeWidth);
 
-            <div className="haveLeftForm">
-                <form>
-
-                    <label>
-                        <p>name</p>
-                        <img src={inputbg} alt="fashion-printing" />
-                        <div><input type="text" placeholder='name' /></div>
-                    </label>
-
-                    <label>
-                        <p>email</p>
-                        <img src={inputbg} alt="embroidery" />
-                        <div><input type="email" placeholder='email' /></div>
-                    </label>
-
-                    <label>
-                        <p>message</p>
-                        <img src={inputbg} alt="textile" />
-                        <div><input type="text" placeholder='message' /></div>
-                    </label>
-
-                    <div class="seeAll blackbutton haveButton">
-                        <button class="seeAllBtn" type='submit'>
-                            <div class="request_quote seeAllInside raq">
-                                <span class="btn-paint-s"></span>
-                                <span class="btn-paint-s"></span>
-                                <span class="btn-paint-s"></span>
-                                <span class="btn-paint-s"></span>
-                                <span class="btn-paint-s"></span>
-                                <span class="btn-label-s">Request a Quote</span>
-                            </div>
-                        </button>
-                    </div>
-
-                </form>
-            </div>
+  const onFocuss1 = () => {
+    setSpan1(true);
+  };
+  const outOfFocuss1 = () => {
+    setSpan1(false);
+  };
+  const onFocuss2 = () => {
+    setSpan2(true);
+  };
+  const outOfFocuss2 = () => {
+    setSpan2(false);
+  };
+  const onFocuss3 = () => {
+    setSpan3(true);
+  };
+  const outOfFocuss3 = () => {
+    setSpan3(false);
+  };
+  return (
+    <div data-aos={width ? "zoom-in" : "fade-right"} className="haveLeft">
+      <div className="haveLeftHeading">
+        <div>
+          <h2>have a project in mind?</h2>
         </div>
-    )
+        <div>
+          <p>Talk a little bit about your project and we will contact you</p>
+        </div>
+      </div>
+
+      <div className="haveLeftForm">
+        <form>
+          <label>
+            <p>name</p>
+            <img src={inputbg} alt="fashion-printing" />
+            <div>
+              <input
+                onFocus={onFocuss1}
+                onBlur={outOfFocuss1}
+                type="text"
+                placeholder="name"
+              />
+              <div>
+                <span className={span1 ? "span" : ""}></span>
+              </div>
+            </div>
+          </label>
+
+          <label>
+            <p>email</p>
+            <img src={inputbg} alt="embroidery" />
+            <div>
+              <input  onFocus={onFocuss2}
+                onBlur={outOfFocuss2} type="email" placeholder="email" />
+             <div>
+                <span className={span2 ? "span" : ""}></span>
+              </div>
+            </div>
+          </label>
+
+          <label>
+            <p>message</p>
+            <img src={inputbg} alt="textile" />
+            <div>
+              <input  onFocus={onFocuss3}
+                onBlur={outOfFocuss3} type="text" placeholder="message" />
+              <div>
+                <span className={span3 ? "span" : ""}></span>
+              </div>
+            </div>
+          </label>
+
+          <div class="seeAll blackbutton haveButton">
+            <button class="seeAllBtn" type="submit">
+              <div class="request_quote seeAllInside raq">
+                <span class="btn-paint-s"></span>
+                <span class="btn-paint-s"></span>
+                <span class="btn-paint-s"></span>
+                <span class="btn-paint-s"></span>
+                <span class="btn-paint-s"></span>
+                <span class="btn-label-s">Request a Quote</span>
+              </div>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default HaveLeft
+export default HaveLeft;
--- src/components/JoinOur/JoinOurLeft.jsx
+++ src/components/JoinOur/JoinOurLeft.jsx
@@ -1,62 +1,96 @@
-import React,{useState} from 'react'
-import inputbg from '../../assets/img/inputbg.webp'
-import DashedLine from '../../assets/img/dashed-line.svg'
+import React, { useState } from "react";
+import inputbg from "../../assets/img/inputbg.webp";
+import DashedLine from "../../assets/img/dashed-line.svg";
 
 function JoinOurLeft() {
-    const [width,setWidth]=useState(true)
-    const changeWidth=()=>{
-        if(window.screen.width>=1024){
-          setWidth(false)
-        }
-        else{
-            setWidth(true)
-        }
+  const [width, setWidth] = useState(true);
+  const [span1, setSpan1] = useState(false);
+  const [span2, setSpan2] = useState(false);
+  const changeWidth = () => {
+    if (window.screen.width >= 1024) {
+      setWidth(false);
+    } else {
+      setWidth(true);
     }
-    window.addEventListener('scroll',changeWidth)
-    return (
-        <div data-aos={width?"zoom-in":"fade-right"} className='joinLeft'>
-            <div className="joinLeftHeading">
-                <h2>Join to our</h2>
+  };
+  window.addEventListener("scroll", changeWidth);
+  const onFocuss1 = () => {
+    setSpan1(true);
+  };
+  const outOfFocuss1 = () => {
+    setSpan1(false);
+  };
+  const onFocuss2 = () => {
+    setSpan2(true);
+  };
+  const outOfFocuss2 = () => {
+    setSpan2(false);
+  };
+  return (
+    <div data-aos={width ? "zoom-in" : "fade-right"} className="joinLeft">
+      <div className="joinLeftHeading">
+        <h2>Join to our</h2>
+      </div>
+
+      <div className="joinLeftForm">
+        <form>
+          <label>
+            <p>name</p>
+
+            <img className="inputbg" src={inputbg} alt="uk-print-workshop" />
+
+            <div>
+              <input
+                onFocus={onFocuss1}
+                onBlur={outOfFocuss1}
+                type="text"
+                placeholder="Name"
+              />
+              <div>
+                <span className={span1 ? "span" : ""}></span>
+              </div>
             </div>
+          </label>
 
-            <div className="joinLeftForm">
-                <form>
+          <label>
+            <p>email</p>
 
-                    <label>
-                        <p>name</p>
-                        <img className='inputbg' src={inputbg} alt="uk-print-workshop" />
-                        <input type="text" placeholder='Name'/>
-                    </label>
-                   
-                    <label>
-                        <p>email</p>
-                        <img className='inputbg' src={inputbg} alt="meshroom" />
-                        <input type="email" placeholder='email' />
-                    </label>
+            <img className="inputbg" src={inputbg} alt="meshroom" />
 
-                    <div className="mainButtonBack joinButton">
-                        <button type='submit' className="mainButton">
-                            <div className="mainButtonInside joinButtonInside">
-                                <span className="btn-paint-main"></span>
-                                <span className="btn-paint-main"></span>
-                                <span className="btn-paint-main"></span>
-                                <span className="btn-paint-main"></span>
-                                <span className="btn-paint-main"></span>
-                                <span className="btn-paint-main"></span>
-                                <p className="btn-label-p jointext">Join</p>
-                            </div>
-                        </button>
-                    </div>
-
-                </form>
+            <div>
+              <input
+                onFocus={onFocuss2}
+                onBlur={outOfFocuss2}
+                type="email"
+                placeholder="email"
+              />
+              <div>
+                <span className={span2 ? "span" : ""}></span>
+              </div>
             </div>
+          </label>
 
-            <div className='joinLeftDashedLine'>
-                <img src={DashedLine} alt="sublimation-printing" />
-            </div>
-        </div>
-        
-    )
+          <div className="mainButtonBack joinButton">
+            <button type="submit" className="mainButton">
+              <div className="mainButtonInside joinButtonInside">
+                <span className="btn-paint-main"></span>
+                <span className="btn-paint-main"></span>
+                <span className="btn-paint-main"></span>
+                <span className="btn-paint-main"></span>
+                <span className="btn-paint-main"></span>
+                <span className="btn-paint-main"></span>
+                <p className="btn-label-p jointext">Join</p>
+              </div>
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div className="joinLeftDashedLine">
+        <img src={DashedLine} alt="sublimation-printing" />
+      </div>
+    </div>
+  );
 }
 
-export default JoinOurLeft
+export default JoinOurLeft;
